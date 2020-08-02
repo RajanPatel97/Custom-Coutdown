3237d4716ae50aa3d0b19025313698172e2b703c
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-08-02 17:44:52 +0100

    added form, fonts and background

---
+++ script.js
@@ -0,0 +1,11 @@
+const countdownForm = document.getElementById('countdownForm');
+const inputContainer = document.getElementById('input-container');
+const dateEl = document.getElementById('date-picker');
+
+// Set Date Input Min & Value with Today's Date
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
